6d4e9aaf2c4de77f865ca7c023d054be258478bb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-14 13:33:16 +0200

    maj script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const SCORE0 = document.getElementById('score--0');
-const SCORE1 = document.querySelector('#score--1');
-SCORE0.innerHTML = 0;
-SCORE1.innerHTML = 0;
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
