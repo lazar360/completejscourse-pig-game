2cb25ad2f4eff1e57095f5081e5a02de8948e69a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-21 13:06:06 +0200

    maj script

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('score--0');
+const current1El = document.getElementById('score--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +15,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 // Rolling dice function
 btnRoll.addEventListener('click', function () {
@@ -23,4 +28,10 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `./dice-${dice}.png`;
 
   // 3. Check for rolled 1 if true switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+  } else {
+    // switch to next player
+  }
 });
